22cd9a0097776d61527eba163606ddc0c5b6fe51
Petr <petr@petrs-air.localdomain>
2017-11-03 00:55:59 +0100

    intermediate status 2

--- Park.ly/ViewController.swift
+++ Park.ly/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var parkBtn: RoundButton!
     
+    let regionRadius: CLLocationDistance = 500
     var parkedCarAnnotation: ParkingSpot?
     
     override func viewDidLoad() {
@@ -27,7 +28,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
    
     }
     
-   // func checkLocationAuthorizationStatus() {
+    func checkLocationAuthorizationStatus() {
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
             mapView.showsUserLocation = true
             LocationService.instance.locationManager.delegate = self
@@ -38,7 +39,30 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             LocationService.instance.locationManager.requestWhenInUseAuthorization()
         }
     }
-
+    
+    func centerMapOnLocation(location: CLLocation) {
+        
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2, regionRadius * 2)
+        mapView.setRegion(coordinateRegion, animated: true)
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let annotation = annotation as? ParkingSpot {
+            let identifier = "pin"
+            var view: MKPinAnnotationView
+            view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.animatesDrop = true
+            view.pinTintColor = UIColor.orange
+            view.calloutOffset = CGPoint(x: -8, y: -3)
+            view.rightCalloutAccessoryView = UIButton.init(type: .detailDisclosure) as UIView
+            return view
+            
+        } else {
+            return nil
+        }
+    }
     
 }
 
